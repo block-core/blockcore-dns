ea232e369dd54514f8928afd816e3ae26806bd54
dangershony <dan.gershony@gmail.com>
2022-05-26 20:17:31 +0100

    Add some comments on the settings class

--- src/Blockcore.Dns/Agent/AgentSettings.cs
+++ src/Blockcore.Dns/Agent/AgentSettings.cs
@@ -1,19 +1,59 @@
 ﻿namespace Blockcore.Dns.Agent
 {
+    /// <summary>
+    /// Setting for a DNS agent.
+    /// </summary>
     public class AgentSettings
     {
+        /// <summary>
+        /// A private key that will be used as a DID to sign requests to the DNS server.
+        /// </summary>
         public string Secret { get; set; }
+
+        /// <summary>
+        /// How often to notify the DNS server about our IP address.
+        /// </summary>
         public int IntervalMinutes { get; set; } = 5;
+
+        /// <summary>
+        /// The Time to Live use by DNS server to cache IP addresses.
+        /// </summary>
         public int DnsttlMinutes { get; set; } = 20;
+
+        /// <summary>
+        /// Domain data representing services that we want the DNS agent to register with the DNS server.
+        /// </summary>
         public AgentHost[] Hosts { get; set; }
     }
 
+    /// <summary>
+    /// Meta data that represents local running services and the DNS server we wish to register them with
+    /// </summary>
     public class AgentHost
     {
+        /// <summary>
+        /// The DNS server to register domain data with.
+        /// </summary>
         public string DnsHost { get; set; }
+
+        /// <summary>
+        /// The domain of the local service.
+        /// </summary>
         public string Domain { get; set; }
+
+        /// <summary>
+        /// The local port the service is running under (to allow access via ip/port not just domain.
+        /// </summary>
         public int Port { get; set; }
+
+        /// <summary>
+        /// The coin symbol this service represents.
+        /// </summary>
         public string Symbol { get; set; }
+
+        /// <summary>
+        /// The type of service that is represented locally.
+        /// </summary>
         public string Service { get; set; }
     }
 }
--- src/Blockcore.Dns/DnsSettings.cs
+++ src/Blockcore.Dns/DnsSettings.cs
@@ -1,11 +1,33 @@
 ﻿namespace Blockcore.Dns
 {
+    /// <summary>
+    /// Settings for a DNS server.
+    /// </summary>
     public class DnsSettings
     {
+        /// <summary>
+        /// DNS Port to listen for DNS queries (usually port 53) 
+        /// </summary>
         public int ListenPort { get; set; } = 53;
+
+        /// <summary>
+        /// How often to check that registered services are on-line (if they are off line they get unregistered).
+        /// </summary>
         public int IntervalMinutes { get; set; } = 5;
+
+        /// <summary>
+        /// What local IP to listen on (this seems to not be needed)
+        /// </summary>
         public string EndServerIp { get; set; }
+
+        /// <summary>
+        /// Identities in the form of DID (Decentralized Identifier) that are authorized to make registration requests.
+        /// </summary>
         public string[] Identities { get; set; }
+
+        /// <summary>
+        /// A flag that specifies whether registration is required.
+        /// </summary>
         public bool VerifyIdentity { get; set; } = true;
     }
 }
